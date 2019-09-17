bfc125d3cccd86a5c9722f1435388f842ce7cc04
Romuald JEANNE <romuald.jeanne@st.com>
2019-09-17 16:19:45 +0200

    QT: Force screen size for HDMI plus add new environment variables
    Change-Id: I678ae53bc0b70e364c16d2f237217789e699a1f0
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@st.com>

--- recipes-qt/qt5/files/qt-eglfs.sh
+++ recipes-qt/qt5/files/qt-eglfs.sh
@@ -5,4 +5,25 @@ if [ -e /usr/share/qt5/cursor.json ];
 then
 	export QT_QPA_EGLFS_KMS_CONFIG=/usr/share/qt5/cursor.json
 fi
+# force to keep the MODE SETTING set
+export QT_QPA_EGLFS_ALWAYS_SET_MODE=1
+#force to use KMS ATOMIC
+export QT_QPA_EGLFS_KMS_ATOMIC=1
 
+# EGLFS environment variables accessible for qt 5.12
+# Documentation: https://doc.qt.io/qt-5/embedded-linux.html
+##
+# * QT_QPA_EGLFS_ROTATION
+# Specifies the rotation applied to software-rendered content in QWidget-based applications
+#
+# * QT_QPA_EGLFS_KMS_ATOMIC
+# enable the DRM atomic
+#
+# * QT_QPA_EGLFS_HIDECURSOR
+# The mouse cursor shows up whenever this variable is not set
+#
+# * QT_QPA_EGLFS_ALWAYS_SET_MODE
+# Due to the fact that modesetting is done only when the desired mode is actually
+# different from the active one (unless forced via the QT_QPA_EGLFS_ALWAYS_SET_MODE
+# environment variable), this value is useful to keep the current mode and any content
+# in the planes not touched by Qt.
