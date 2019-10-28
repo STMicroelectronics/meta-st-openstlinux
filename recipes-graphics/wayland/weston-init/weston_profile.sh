1f5667d6e9f079348a11c860814bd880c2d13246
Lionel VITTE <lionel.vitte@st.com>
2019-10-28 11:10:58 +0100

    WESTON-INIT: create folder in /run/user only for root
    Change-Id: I95ceb5d1881a284c4c9ca5cac8b3c0ee49ae837c

--- recipes-graphics/wayland/weston-init/weston_profile.sh
+++ recipes-graphics/wayland/weston-init/weston_profile.sh
@@ -6,8 +6,8 @@ export ECORE_EVAS_ENGINE=wayland_shm
 export ECORE_EVAS_ENGINE=wayland_shm
 export GDK_BACKEND=wayland
 
-mkdir --parents $XDG_RUNTIME_DIR
-chmod 0700 $XDG_RUNTIME_DIR
-
-
+if [ $(id -u) -eq 0 ]; then
+    mkdir --parents $XDG_RUNTIME_DIR
+    chmod 0700 $XDG_RUNTIME_DIR
+fi
 
