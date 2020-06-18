468631fed2fe8dafc77cbc52adba910950cadcca
Romuald JEANNE <romuald.jeanne@st.com>
2020-06-18 11:56:32 +0200

    PULSEAUDIO: update to 13.0
    Change-Id: Id34824b00cff08df8d781217fd3f290bc0c2e1c6

--- recipes-multimedia/pulseaudio/pulseaudio/pulse_profile.sh
+++ recipes-multimedia/pulseaudio/pulseaudio/pulse_profile.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-chown -R root:root /home/root
 dbusinfo=$(dbus-launch)
 DBUS_SESSION_BUS_ADDRESS=$(dbus-launch |grep DBUS_SESSION_BUS_ADDRESS| cut -d'=' -f 2-)
 DBUS_SESSION_BUS_PID=$(dbus-launch |grep DBUS_SESSION_BUS_PID |cut -d'=' -f2)
