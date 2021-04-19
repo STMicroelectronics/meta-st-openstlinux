5e03f4aee34035ce794d4a5cab27854284c93df4
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2021-04-19 11:11:35 +0200

    DEMO: add support without GPU enabled
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>

--- recipes-samples/demo-application/demo-application-camera/launch_camera_preview.sh
+++ recipes-samples/demo-application/demo-application-camera/launch_camera_preview.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 v4l2-ctl --set-parm=20
-/usr/local/demo/bin/touch-event-gtk-player --graph "v4l2src io-mode=4 ! video/x-raw,width=640,height=480 ! queue ! waylandsink fullscreen=true" 2> /dev/null > /dev/null
+
+# get screen size
+SCREEN_WIDTH=$(weston-info | grep logical_width | sed -r "s/logical_width: ([0-9]+),.*/\1/")
+SCREEN_HEIGHT=$(weston-info | grep logical_width | sed -r "s/.*logical_height: ([0-9]+).*/\1/")
+
+if [ $SCREEN_HEIGHT -ge 480 ];
+then
+	WIDTH=640
+	HEIGHT=480
+else
+	WIDTH=424
+	HEIGHT=240
+fi
+if [ -f /etc/default/weston ] && $(grep "^OPTARGS" /etc/default/weston | grep -q "use-pixman" ) ;
+then
+	echo "Without GPU"
+	ADDONS="videoconvert ! queue !"
+else
+	echo "With GPU"
+	ADDONS=""
+fi
+/usr/local/demo/bin/touch-event-gtk-player --graph "v4l2src io-mode=4 ! video/x-raw,width=$WIDTH,height=$HEIGHT ! queue ! $ADDONS waylandsink fullscreen=true" 2> /dev/null > /dev/null
--- recipes-samples/demo-application/demo-application-video/launch_video.sh
+++ recipes-samples/demo-application/demo-application-video/launch_video.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 #gst-launch-1.0 playbin uri=file:///usr/local/demo/media/ST2297_visionv3.webm video-sink="waylandsink"
-/usr/local/demo/bin/touch-event-gtk-player -F file:///usr/local/demo/media/ST2297_visionv3.webm 2> /dev/null > /dev/null
+if [ -f /etc/default/weston ] && $(grep "^OPTARGS" /etc/default/weston | grep -q "use-pixman" ) ;
+then
+	echo "Without GPU"
+	ADDONS="videoconvert ! queue !"
+else
+	echo "With GPU"
+	ADDONS=""
+fi
+#/usr/local/demo/bin/touch-event-gtk-player -F file:///usr/local/demo/media/ST2297_visionv3.webm 2> /dev/null > /dev/null
+/usr/local/demo/bin/touch-event-gtk-player --graph "playbin uri=file:///usr/local/demo/media/ST2297_visionv3.webm video-sink='$ADDONS waylandsink fullscreen=true' " 2> /dev/null > /dev/null
+
+
