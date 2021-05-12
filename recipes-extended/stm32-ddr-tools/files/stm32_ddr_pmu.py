60f8423902184b38dbd414fdd810bfe587cdbbf6
Gerald Baeza <gerald.baeza@foss.st.com>
2021-05-12 16:05:44 +0200

    EXTENDED: extend stm32_ddr_pmu.py compatibility
    Extend stm32_ddr_pmu.py compatibility to support
    the whole part numbers inside STM32MP1 series
    
    Signed-off-by: Gerald Baeza <gerald.baeza@foss.st.com>
    Change-Id: I3ac75557c2f891c429745600aba30faeb6cb70b8

--- recipes-extended/stm32-ddr-tools/files/stm32_ddr_pmu.py
+++ recipes-extended/stm32-ddr-tools/files/stm32_ddr_pmu.py
@@ -4,16 +4,23 @@ import getopt
 import re
 import sys
 
+with open('/proc/device-tree/compatible', 'r') as compatible:
+    if 'stm32mp15' in compatible.read():
+        word_length  = 4    # Bytes
+    else:
+        word_length  = 2    # Bytes
+
+burst_length = 8    # Words
+
 def usage():
     print("Usage:")
     print("  python stm32_ddr_pmu.py [-d <ddr_freq>] -f <perf_file>")
     print("    -d ddr_freq: DDR frequency in MHz (533 MHz by default)")
     print("    -f perf_file: text file containing the output of")
     print("        perf stat -e stm32_ddr_pmu/read_cnt/,stm32_ddr_pmu/time_cnt/,stm32_ddr_pmu/write_cnt/ -a -o <perf_file> <command>")
+    print("The script considers bursts of %s words with %s bytes per word." % (burst_length, word_length))
     sys.exit(2)
 
-word_length  = 4    # Bytes
-burst_length = 8    # Words
 ddr_freq = 533
 perf_file = None
 dic = {}
