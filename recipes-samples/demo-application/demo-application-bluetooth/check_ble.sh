fdabf3079c1ee011de2550c7a814a7dedf209a85
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2022-06-10 15:51:52 +0200

    DEMO: bluetooth: change way to verify presence of bluettooth
    With the usage of script and su, information about the correct
    execution of hciconfig is not forwarded to main application.
    
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>
    Change-Id: I995113082349c10127e5eea418bdf2b741ba9ac2

--- recipes-samples/demo-application/demo-application-bluetooth/check_ble.sh
+++ recipes-samples/demo-application/demo-application-bluetooth/check_ble.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-script -qc "su -c 'hciconfig hci0 up'"
+script -qc "su -c 'hciconfig hci0 up || echo ko > /tmp/ble'"
+
+if [ -e /tmp/ble ]; then
+    rm /tmp/ble
+    exit 1;
+else
+    exit 0;
+fi
