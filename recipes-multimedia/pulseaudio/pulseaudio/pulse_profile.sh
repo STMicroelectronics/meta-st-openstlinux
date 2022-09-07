99189ae811c5e67f0e413e1d4c10b879405eac8d
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2022-09-07 15:16:31 +0200

    PULSEAUDIO: do not call dbus-launch
    The call to dbus-launch start new dbus deamon each time it's called.
    At least, some memory issue appear due to presence of many dbus process.
    
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>
    Change-Id: Ib27511602ba8b828be37183c66e33911f79b9128

--- recipes-multimedia/pulseaudio/pulseaudio/pulse_profile.sh
+++ recipes-multimedia/pulseaudio/pulseaudio/pulse_profile.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-dbusinfo=$(dbus-launch)
-DBUS_SESSION_BUS_ADDRESS=$(dbus-launch |grep DBUS_SESSION_BUS_ADDRESS| cut -d'=' -f 2-)
-DBUS_SESSION_BUS_PID=$(dbus-launch |grep DBUS_SESSION_BUS_PID |cut -d'=' -f2)
+DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/`id -u weston`/bus
 PULSE_RUNTIME_PATH=/run/user/`id -u weston`
 PULSE_SERVER="unix:/run/user/`id -u weston`/pulse/native"
 
-
 export DBUS_SESSION_BUS_ADDRESS
-export DBUS_SESSION_BUS_PID
 export PULSE_RUNTIME_PATH
 export PULSE_SERVER
