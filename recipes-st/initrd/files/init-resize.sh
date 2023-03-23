69e6c5d4d11246035e7c2dbe151a11c8c652f7c3
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2023-03-23 17:46:10 +0100

    INITRD: RESIZE: resize only on boot device
    Change-Id: I335668fe136e7c3d273755e496655b4bb9b8bbb1
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>

--- recipes-st/initrd/files/init-resize.sh
+++ recipes-st/initrd/files/init-resize.sh
@@ -12,40 +12,58 @@ resize_run() {
     if [ -n "$ROOTFS_DIR" ]; then
         if [ ! -e $ROOTFS_DIR/etc/.resized ]
         then
-            for j in 0 1;
-            do
-                for i in 3 4 5 6 7 8 9 10 11 12 13 14 15 16;
-                do
-                    DEVICE="/dev/mmcblk"$j"p"$i
-                    if [ -e $DEVICE ]; then
-                        label=$(/sbin/e2label $DEVICE 2> /dev/null)
-                        if [ $? -eq 0 ]; then
-                            case $label in
-                            user*)
-                                echo "RESIZE USERFS [$DEVICE]"
-                                /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
-                                ;;
-                            root*)
-                                echo "RESIZE ROOTFS [$DEVICE]"
-                                /sbin/resize2fs $DEVICE
-                                ;;
-                            vendor*)
-                                echo "RESIZE VENDORFS [$DEVICE]"
-                                /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
-                                ;;
-                            boot*)
-                                echo "RESIZE BOOTFS [$DEVICE]"
-                                /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
-                                ;;
-                            *)
-                                ;;
-                            esac
-                        fi
-                    fi
-                done
-            done
+            # check command line to now storage device used
+            if [ -n "$bootparam_root" ]; then
+                debug "No e2fs compatible filesystem has been mounted, mounting $bootparam_root..."
 
-            touch $ROOTFS_DIR/etc/.resized
+                if [ "`echo ${bootparam_root} | cut -c1-5`" = "UUID=" ]; then
+                    root_uuid=`echo $bootparam_root | cut -c6-`
+                    bootparam_root="/dev/disk/by-uuid/$root_uuid"
+                elif [ "`echo ${bootparam_root} | cut -c1-9`" = "PARTUUID=" ]; then
+                    root_partuuid=`echo $bootparam_root | cut -c10-`
+                    bootparam_root="/dev/disk/by-partuuid/$root_partuuid"
+                elif [ "`echo ${bootparam_root} | cut -c1-10`" = "PARTLABEL=" ]; then
+                    root_partlabel=`echo $bootparam_root | cut -c11-`
+                    bootparam_root="/dev/disk/by-partlabel/$root_partlabel"
+                elif [ "`echo ${bootparam_root} | cut -c1-6`" = "LABEL=" ]; then
+                    root_label=`echo $bootparam_root | cut -c7-`
+                    bootparam_root="/dev/disk/by-label/$root_label"
+                fi
+                if [ -e "$bootparam_root" ]; then
+                    bootparam_root_device=$(busybox readlink $bootparam_root -f)
+                    j=$(echo $bootparam_root_device | sed "s|/dev/mmcblk\([0-1]\)p.*|\1|")
+                    for i in 3 4 5 6 7 8 9 10 11 12 13 14 15 16;
+                    do
+                        DEVICE="/dev/mmcblk"$j"p"$i
+                        if [ -e $DEVICE ]; then
+                            label=$(/sbin/e2label $DEVICE 2> /dev/null)
+                            if [ $? -eq 0 ]; then
+                                case $label in
+                                user*)
+                                    echo "RESIZE USERFS [$DEVICE]"
+                                    /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
+                                    ;;
+                                root*)
+                                    echo "RESIZE ROOTFS [$DEVICE]"
+                                    /sbin/resize2fs $DEVICE
+                                    ;;
+                                vendor*)
+                                    echo "RESIZE VENDORFS [$DEVICE]"
+                                    /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
+                                    ;;
+                                boot*)
+                                    echo "RESIZE BOOTFS [$DEVICE]"
+                                    /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
+                                    ;;
+                                *)
+                                    ;;
+                                esac
+                            fi
+                        fi
+                    done
+                    touch $ROOTFS_DIR/etc/.resized
+                fi
+            fi
         fi
     fi
 }
