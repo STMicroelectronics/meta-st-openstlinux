9e7c47762c50b2a484b6a6ee4689c92f783e7025
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2025-02-03 14:58:36 +0100

    ST-HOSTNAME: launch service after network target
    Change-Id: I52f71915e3d8fa8bdfec56b9428844aa0a73c4ea
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/oeivi/oe/st/meta-opensdk/+/433885
    Reviewed-by: Christophe PRIOUZEAU <christophe.priouzeau@st.com>
    Tested-by: Christophe PRIOUZEAU <christophe.priouzeau@st.com>

--- recipes-bsp/tools/st-hostname/st-hostname.sh
+++ recipes-bsp/tools/st-hostname/st-hostname.sh
@@ -7,6 +7,10 @@ PREVIOUS_HOST=$(cat /etc/hostname)
 if [ -z $MAC ]; then
     MAC=unknown
 fi
+if $(cat /etc/hostname | grep -qe "unknown"); then
+    # reset hostname
+    sed -i "s/unknown//" /etc/hostname
+fi
 if $(cat /etc/hostname | grep -qe "$MAC"); then
     #echo "[DEBUG] mac address already present"
     :
@@ -14,8 +18,8 @@ if $(cat /etc/hostname | grep -qe "$MAC"); then
 else
     #echo "[DEBUG] mac address NOT present"
     echo ${PREVIOUS_HOST}${MAC} > /etc/hostname
-    hostnamectl hostname ${PREVIOUS_HOST}_${MAC}
-    hostnamectl --transient hostname ${PREVIOUS_HOST}_${MAC}
+    hostnamectl hostname ${PREVIOUS_HOST}${MAC}
+    hostnamectl --transient hostname ${PREVIOUS_HOST}${MAC}
     if $(grep -q SendHostname /usr/lib/systemd/network/80-wired.network) ; then
         sed -i "s|Hostname=.*$|Hostname=${PREVIOUS_HOST}${MAC}|" /usr/lib/systemd/network/80-wired.network
     else
